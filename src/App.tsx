d9b104b52f7791dfd34cf115d18dda3b21c09670
midman001 <midlmancrypto@gmail.com>
2026-02-10 18:07:53 +0100

    Add Vercel Analytics for site usage tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import Hero from './components/Hero';
 import Mission from './components/Mission';
 import Timeline from './components/Timeline';
@@ -17,6 +18,7 @@ function App() {
       <Toolkit />
       <CTASection />
       <Footer />
+      <Analytics />
     </div>
   );
 }
